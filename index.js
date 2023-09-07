72ab295afbd86e98d828399dd2c26ee4af777e92
Lennert Van Weyenberg <xanderlennert12@gmail.com>
2023-09-07 16:09:59 +0200

    Vercel5

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 const ejsMate = require("ejs-mate");
 const path = require("path");
+const cors = require("cors");
 
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+	cors({
+		origin: ["https://website-mama-progress-xsgn.vercel.app"],
+		methods: ["post", "get"],
+		credentials: true,
+	})
+);
 
 app.all("*", (req, res) => {
 	res.render("./pages/home.ejs");
